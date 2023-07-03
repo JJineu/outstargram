151bd7ba29148cdf2ac2da253f8745cbdd7527f7
joice <joice.kim7@gmail.com>
2023-07-03 17:51:31 +0900

    fix: /hooks/user userId type

--- src/hooks/user.ts
+++ src/hooks/user.ts
@@ -18,7 +18,7 @@ export function useUser(userId: string) {
   } = useSWR<HomeUser>(`/api/user/${userId}`);
 
   const setFollow = useCallback(
-    (username: string, userId: boolean) => {
+    (username: string, userId: string) => {
       if (!username || !userId) return;
       const followers = user?.followers ?? [];
       const newUser = {
@@ -27,7 +27,7 @@ export function useUser(userId: string) {
       };
 
       return mutate(updateFollow(username, userId), {
-        optimisticData: newUser,
+        // optimisticData: newUser,
         populateCache: false,
         revalidate: false,
         rollbackOnError: true,
@@ -37,44 +37,3 @@ export function useUser(userId: string) {
   );
   return { user, isLoading, error, setFollow };
 }
-
-// import { Comment, FullPost } from "@/model/post";
-// import { useCallback } from "react";
-// import useSWR, { useSWRConfig } from "swr";
-
-// async function addComment(id: string, comment: string) {
-//   return fetch("/api/comments", {
-//     method: "POST",
-//     body: JSON.stringify({ id, comment }),
-//   }).then((res) => res.json());
-// }
-
-// export default function useFullPosts(postId: string) {
-//   const {
-//     data: post,
-//     isLoading,
-//     error,
-//     mutate,
-//   } = useSWR<FullPost>(`/api/post/${postId}`);
-
-//   const { mutate: globalMutate } = useSWRConfig();
-
-//   const postComment = useCallback(
-//     (comment: Comment) => {
-//       if (!post) return;
-//       const newPost = {
-//         ...post,
-//         comments: [...post.comments, comment],
-//       };
-
-//       return mutate(addComment(post.id, comment.comment), {
-//         optimisticData: newPost,
-//         populateCache: false,
-//         revalidate: false,
-//         rollbackOnError: true,
-//       }).then(() => globalMutate("/api/post"));
-//     },
-//     [post, mutate, globalMutate]
-//   );
-//   return { post, isLoading, error, postComment };
-// }
